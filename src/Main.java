538266405e149ed6388732bc104664234c4c8c9c
Gri1023 <hryhorii.bavykin@gmail.com>
2022-09-11 18:42:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumUpTo(3));
+    }
+    static int sumUpTo (int n) {
+    return((n*(n+1))/2);
+    }
+
+}
